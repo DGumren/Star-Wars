7c82ed1cff24c5b1d0fcfcb8148add15d368a53a
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-26 15:26:05 -0500

    In Progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import StarWars from "./component/StarWars";
+import "../src/App.css";
+import Card from "./component/Card";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <StarWars />
+      <div className="row">
+        <h3>
+          <font color="white">Characters</font>
+        </h3>
+      </div>
+      <div className="d-flex flex-row">
+        <Card />
+      </div>
+      <div className="row">
+        <h3>
+          <font color="white">Planets</font>
+        </h3>
+      </div>
+      <div className="d-flex flex-row">
+        <Card />
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
